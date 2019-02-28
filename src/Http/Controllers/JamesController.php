4f8c2debcea1ea33ecf91c6205dd3c702230808a
james.xue <james.etocrm.com>
2019-02-28 10:23:03 +0800

    'fix'

--- src/Http/Controllers/JamesController.php
+++ src/Http/Controllers/JamesController.php
@@ -7,6 +7,7 @@ use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Lang;
 
 class JamesController extends Controller
 {
